aa7fb77a9093cefaabc9ed23d160ec9670b26751
Nathan Ellis <nathan@ellisn.com>
2023-08-03 09:45:34 +0100

    Updated structure of code and added link feature
    Can now provide LINK as a variable for you to direct yourself to any web page upon clicking the notification on your phone!

--- app/index.js
+++ app/index.js
@@ -1,38 +1,41 @@
 const request = require("request");
 const fs = require("fs");
+
 const API_URL = `https://cloud-notify.ellisn.com/api/notify?`;
 
-const eventPayload = JSON.parse(
-  fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8")
-);
+const { GITHUB_EVENT_PATH, MESSAGE, TITLE, TOKEN, USERID, GITHUB_REPOSITORY, LINK } = process.env;
+
+let eventPayload;
+try {
+  eventPayload = JSON.parse(fs.readFileSync(GITHUB_EVENT_PATH, "utf8"));
+} catch (err) {
+  console.error("Error reading event payload:", err);
+  process.exit(1);
+}
 
 const message = {
-  body: process.env.MESSAGE || "No message specified",
-  title:
-    process.env.TITLE ||
-    `GitHub Notification from ${process.env.GITHUB_REPOSITORY}`,
+  body: MESSAGE || "No message specified",
+  title: TITLE || `GitHub Notification from ${GITHUB_REPOSITORY}`,
 };
 
-if (!process.env.TOKEN) {
-  return console.error("CLOUD_NOTIFY_API_TOKEN is missing");
+if (!TOKEN) {
+  console.error("CLOUD_NOTIFY_API_TOKEN is missing");
+  process.exit(1);
 }
 
-if (!process.env.USERID) {
-  return console.error("CLOUD_NOTIFY_USERID_KEY is missing");
+if (!USERID) {
+  console.error("CLOUD_NOTIFY_USERID_KEY is missing");
+  process.exit(1);
 }
 
-const newURLString = `${API_URL}userID=${process.env.USERID}&token=${process.env.TOKEN}&title=${message.title}&body=${message.body}`;
-const newURL = encodeURI(newURLString.trim());
+let newURL = `${API_URL}userID=${USERID}&token=${TOKEN}&title=${encodeURIComponent(message.title)}&body=${encodeURIComponent(message.body)}`;
 
-request(
-  {
-    url: newURL,
-    method: "GET",
-  },
-  (err, response) => {
-    if (err) {
-      return console.error(err.toString());
-    }
-    console.log("Notification sent! Response from server: ", response.body);
+newURL += LINK ? `&link=${encodeURIComponent(LINK)}` : '';
+
+request({ url: newURL, method: "GET" }, (err, response) => {
+  if (err) {
+    console.error(err.toString());
+  } else {
+    console.log("Notification sent! Response from server:", response.body);
   }
-);
+});
